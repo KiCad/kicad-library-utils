0adacfa19d91ecf1e1e2285c88cbcb06bf15bf92
Ricardo Crudo <ricardo.crudo@gmail.com>
2015-03-03 15:58:33 -0300

    MPN script: adding options to define fields names

--- sch/add_part_number.py
+++ sch/add_part_number.py
@@ -4,32 +4,39 @@
 from __future__ import print_function
 from sch import *
 import argparse
-import csv
+import csv, sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('sch_file', nargs='+')
-parser.add_argument('--csv', help='a csv file that will be used to fill the MPN field', action='store')
+parser.add_argument('--bom-csv', help='a BOM csv file that will be used to fill the MPN field', action='store')
+parser.add_argument('--bom-ref-field', help='defines the reference field name in the BOM', action='store', default='Reference(s)')
+parser.add_argument('--bom-mpn-field', help='defines the Manufacturer PN field name in the BOM', action='store', default='MPN')
 args = parser.parse_args()
 bom = []
 
-if args.csv:
-    f = open(args.csv, 'r')
+if args.bom_csv:
+    f = open(args.bom_csv, 'r')
     reader = csv.reader(f)
     header_ok = False
     for row in reader:
         # Try to locate the header row
-        if 'Reference(s)' in row and 'MPN' in row:
+        if args.bom_ref_field in row and args.bom_mpn_field in row:
             header_ok = True
-            ref_col = row.index('Reference(s)')
-            mpn_col = row.index('MPN')
+            ref_col = row.index(args.bom_ref_field)
+            mpn_col = row.index(args.bom_mpn_field)
 
         elif header_ok:
             bom.append(row)
+
+    if not header_ok:
+        sys.stderr.write('Cannot find a header with one or both fields: "%s", "%s"\n' % (args.bom_ref_field, args.bom_mpn_field))
+        sys.exit()
 
 for f in args.sch_file:
     sch = Schematic(f)
 
     for component in sch.components:
+        # check if is power related component
         if ('#PWR' in component.fields[0]['ref'] or
             'PWR_FLAG' in component.fields[1]['ref']):
             continue
