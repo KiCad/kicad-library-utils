d2a619385c01beb4a7171f5e01b9bfec24e47a3e
Ricardo Crudo <ricardo.crudo@gmail.com>
2015-03-04 07:43:00 -0300

    generalizing MPN field to PN

--- sch/add_part_number.py
+++ sch/add_part_number.py
@@ -8,9 +8,10 @@ import csv, sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('sch_file', nargs='+')
-parser.add_argument('--bom-csv', help='a BOM csv file that will be used to fill the MPN field', action='store')
+parser.add_argument('--bom-csv', help='a BOM csv file that will be used to fill the PN field', action='store')
 parser.add_argument('--bom-ref-field', help='defines the reference field name in the BOM', action='store', default='Reference(s)')
-parser.add_argument('--bom-mpn-field', help='defines the Manufacturer PN field name in the BOM', action='store', default='MPN')
+parser.add_argument('--bom-pn-field', help='defines the part number field name in the BOM', action='store', default='MPN')
+parser.add_argument('--pn-field-name', help='defines the part number field name that will be used in the sch files', action='store', default='MPN')
 args = parser.parse_args()
 bom = []
 
@@ -20,16 +21,16 @@ if args.bom_csv:
     header_ok = False
     for row in reader:
         # Try to locate the header row
-        if args.bom_ref_field in row and args.bom_mpn_field in row:
+        if args.bom_ref_field in row and args.bom_pn_field in row:
             header_ok = True
             ref_col = row.index(args.bom_ref_field)
-            mpn_col = row.index(args.bom_mpn_field)
+            pn_col = row.index(args.bom_pn_field)
 
         elif header_ok:
             bom.append(row)
 
     if not header_ok:
-        sys.stderr.write('Cannot find a header with one or both fields: "%s", "%s"\n' % (args.bom_ref_field, args.bom_mpn_field))
+        sys.stderr.write('Cannot find a header with one or both fields: "%s", "%s"\n' % (args.bom_ref_field, args.bom_pn_field))
         sys.exit()
 
 for f in args.sch_file:
@@ -41,21 +42,21 @@ for f in args.sch_file:
             'PWR_FLAG' in component.fields[1]['ref']):
             continue
 
-        # create or get the MPN field
+        # create or get the PN field
         for field in component.fields:
-            if field['name'] == '"MPN"':
+            if field['name'].replace('"','') == args.pn_field_name:
                 break
         else:
-            field = component.addField({'name':'"MPN"', 'ref':'"~"'})
+            field = component.addField({'name':'"%s"' % args.pn_field_name, 'ref':'"~"'})
 
         # component reference
         comp_ref = component.fields[0]['ref'].replace('"','')
 
-        # search the component in the BOM items and get the MPN
+        # search the component in the BOM items and get the PN
         for item in bom:
             item_refs = item[ref_col].replace(' ','').split(',')
-            if item[mpn_col] and comp_ref in item_refs:
-                field['ref'] = '"' + item[mpn_col] + '"'
+            if item[pn_col] and comp_ref in item_refs:
+                field['ref'] = '"' + item[pn_col] + '"'
                 break
 
     sch.save()
