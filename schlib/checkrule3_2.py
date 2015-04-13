206d2b99e7810e22447a92dc54a869f3a462316a
Ricardo Crudo <ricardo.crudo@gmail.com>
2015-04-14 00:29:53 +0200

    bugfix: schlib: ignores rule if pin length is zero

--- schlib/checkrule3_2.py
+++ schlib/checkrule3_2.py
@@ -6,6 +6,7 @@ def select_violating_pins(component):
     violating_pins = []
     for pin in component.pins:
         length = int(pin['length'])
+        if length == 0: continue
         if (length < 100) or (length - 100) % 50 != 0:
             violating_pins.append(pin)
     return violating_pins
