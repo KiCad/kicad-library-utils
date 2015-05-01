ce490bb8812584a3dcac60f8a26f69f40ed3b9b8
Ricardo Crudo <ricardo.crudo@gmail.com>
2015-05-01 19:19:15 +0200

    schlib: adding nocolor option

--- schlib/checklib.py
+++ schlib/checklib.py
@@ -8,53 +8,56 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Execute checkrule scripts checking 3.* KLC rules in the libraries')
 parser.add_argument('libfiles', nargs='+')
+parser.add_argument('--nocolor', help='does not use colors to show the output', action='store_true')
 parser.add_argument('-v', '--verbose', help='show status of all components and extra information about the violation', action='store_true')
 args = parser.parse_args()
 
+printer = PrintColor(use_color = not args.nocolor)
+
 for libfile in args.libfiles:
     lib = SchLib(libfile)
-    print_purple('library: %s' % libfile)
+    printer.purple('library: %s' % libfile)
     for component in lib.components:
         check3_1 = checkrule3_1.check_rule(component)
         check3_2 = checkrule3_2.check_rule(component)
         check3_6 = checkrule3_6.check_rule(component)
 
         if check3_1 or check3_2 or check3_6.count([]) < 3:
-            print_green('component: %s' % component.name)
+            printer.green('component: %s' % component.name)
 
             if check3_1:
-                print_yellow('\tViolations of rule 3.1')
+                printer.yellow('\tViolations of rule 3.1')
                 if args.verbose:
-                    print_light_blue('\tUsing a 100mils grid, pin ends and origin must lie on grid nodes (IEC-60617).')
+                    printer.light_blue('\tUsing a 100mils grid, pin ends and origin must lie on grid nodes (IEC-60617).')
 
                 for pin in check3_1:
-                    print_white('\t\tpin: %s (%s), posx %s, posy %s, length: %s' %
+                    printer.white('\t\tpin: %s (%s), posx %s, posy %s, length: %s' %
                         (pin['name'], pin['num'], pin['posx'], pin['posy'], pin['length']))
 
             if check3_2:
-                print_yellow('\tViolations of rule 3.2')
+                printer.yellow('\tViolations of rule 3.2')
                 if args.verbose:
-                    print_light_blue('''\tFor black-box symbols, pins have a length of 100mils. Large pin numbers can be\n\taccomodated by incrementing the width in steps of 50mil.''')
+                    printer.light_blue('''\tFor black-box symbols, pins have a length of 100mils. Large pin numbers can be\n\taccomodated by incrementing the width in steps of 50mil.''')
 
                 for pin in check3_2:
-                    print_white('\t\tpin: %s (%s), dir: %s, length: %s' %
+                    printer.white('\t\tpin: %s (%s), dir: %s, length: %s' %
                         (pin['name'], pin['num'], pin['direction'], pin['length']))
 
             if check3_6.count([]) < 3:
-                print_yellow('\tViolations of rule 3.6')
+                printer.yellow('\tViolations of rule 3.6')
                 if args.verbose:
-                    print_light_blue('\tField text uses a common size of 50mils.')
+                    printer.light_blue('\tField text uses a common size of 50mils.')
 
                 for field in check3_6[0]:
                     namekey = 'reference' if 'reference' in field else 'name'
-                    print_white('\t\tfield: %s, text_size: %s' %
+                    printer.white('\t\tfield: %s, text_size: %s' %
                         (field[namekey], field['text_size']))
                 for text in check3_6[1]:
-                    print_white('\t\ttext: %s, text_size: %s' %
+                    printer.white('\t\ttext: %s, text_size: %s' %
                          (text['text'], text['text_size']))
                 for pin in check3_6[2]:
-                    print_white('\t\tpin: %s (%s), dir: %s, name_text_size: %s, num_text_size: %s' %
+                    printer.white('\t\tpin: %s (%s), dir: %s, name_text_size: %s, num_text_size: %s' %
                         (pin['name'], pin['num'], pin['direction'], pin['name_text_size'], pin['num_text_size']))
 
         elif args.verbose:
-            print_light_green('component: %s......OK' % component.name)
+            printer.light_green('component: %s......OK' % component.name)
--- schlib/print_color.py
+++ schlib/print_color.py
@@ -3,62 +3,77 @@
 from __future__ import print_function
 import sys, platform
 
-if platform.system() == 'Windows':
-    try:
-        import colorama
-        colorama.init()
-    except:
-        print('To print using colors you have to install colorama')
-        print('Please, execute: "pip install colorama" and try again')
-        sys.exit()
+class PrintColor(object):
+    """
+    A class to print colorized text using ANSI escape sequences
+    """
+    def __init__(self, tab_size=4, use_color=True):
+        self._color = {
+            'regular':'\033[0m',
+            'black':'\033[0;30m',
+            'red':'\033[0;31m',
+            'green':'\033[0;32m',
+            'brown':'\033[0;33m',
+            'blue':'\033[0;34m',
+            'purple':'\033[0;35m',
+            'cyan':'\033[0;36m',
+            'gray':'\033[0;37m',
+            'dark_gray':'\033[1;30m',
+            'light_red':'\033[1;31m',
+            'light_green':'\033[1;32m',
+            'yellow':'\033[1;33m',
+            'light_blue':'\033[1;34m',
+            'light_purple':'\033[1;35m',
+            'light_cyan':'\033[1;36m',
+            'white':'\033[1;37m',
+            }
 
-color = {
-    'regular':'\033[0m',
-    'black':'\033[0;30m',
-    'red':'\033[0;31m',
-    'green':'\033[0;32m',
-    'brown':'\033[0;33m',
-    'blue':'\033[0;34m',
-    'purple':'\033[0;35m',
-    'cyan':'\033[0;36m',
-    'gray':'\033[0;37m',
-    'dark_gray':'\033[1;30m',
-    'light_red':'\033[1;31m',
-    'light_green':'\033[1;32m',
-    'yellow':'\033[1;33m',
-    'light_blue':'\033[1;34m',
-    'light_purple':'\033[1;35m',
-    'light_cyan':'\033[1;36m',
-    'white':'\033[1;37m',
-    }
+        self._tab_size = tab_size
+        self._use_color = use_color
 
-def replace_tabs(s):
-    return s.replace('\t', '    ')
+        if platform.system() == 'Windows':
+            try:
+                import colorama
+                colorama.init()
+            except:
+                print('To print using colors you have to install colorama. Try to install it using: "pip install colorama"')
+                print('[Continuing using no color mode]\n')
+                self._use_color = False
 
-def do_print(func, text):
-    # get the color according to function name
-    s = color[func.__name__.replace('print_','')]
-    # replace tabs by space
-    s += replace_tabs(text)
-    # restore the regular color
-    s += color['regular']
 
-    print(s)
+    def _replace_tabs(self, text):
+        if self._tab_size == 0:
+            return text
 
-def print_regular(text): do_print(print_regular, text)
-def print_black(text): do_print(print_black, text)
-def print_red(text): do_print(print_red, text)
-def print_green(text): do_print(print_green, text)
-def print_brown(text): do_print(print_brown, text)
-def print_blue(text): do_print(print_blue, text)
-def print_purple(text): do_print(print_purple, text)
-def print_cyan(text): do_print(print_cyan, text)
-def print_gray(text): do_print(print_gray, text)
-def print_dark_gray(text): do_print(print_dark_gray, text)
-def print_light_red(text): do_print(print_light_red, text)
-def print_light_green(text): do_print(print_light_green, text)
-def print_yellow(text): do_print(print_yellow, text)
-def print_light_blue(text): do_print(print_light_blue, text)
-def print_light_purple(text): do_print(print_light_purple, text)
-def print_light_cyan(text): do_print(print_light_cyan, text)
-def print_white(text): do_print(print_white, text)
+        return text.replace('\t', ' ' * self._tab_size)
+
+    def _do_print(self, color_name, text):
+        s = self._replace_tabs(text)
+
+        if self._use_color:
+            # get the color according to function name
+            s = self._color[color_name]
+            # replace tabs by space
+            s += self._replace_tabs(text)
+            # restore the regular color
+            s += self._color['regular']
+
+        print(s)
+
+    def regular(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def black(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def red(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def green(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def brown(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def blue(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def purple(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def cyan(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def gray(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def dark_gray(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def light_red(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def light_green(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def yellow(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def light_blue(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def light_purple(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def light_cyan(self, text): self._do_print(sys._getframe().f_code.co_name, text)
+    def white(self, text): self._do_print(sys._getframe().f_code.co_name, text)
