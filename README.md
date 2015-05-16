621ab9df5556fe27099af5ce64e8f236f2770399
DrLuke <Lukas@Jackowski.de>
2015-05-16 23:40:53 +0200

    stm32-librarygen: Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#stm32 library generator
+This script turns XML files defining pins for STM32 devices into KiCAD libraries
---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+import 
+
+def main():
+    args = sys.argv
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
