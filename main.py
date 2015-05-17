8f4216b6e6a376e6049b31d7ba9a80ee337424e6
DrLuke <Lukas@Jackowski.de>
2015-05-17 06:01:57 +0200

    stm32-librarygen: Add basic component output

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class device:
         self.pins = []
 
         self.readxml()
+        self.createComponent()
 
     def readxml(self):
         self.tree = etree.parse(self.xmlfile)
@@ -41,18 +42,46 @@ class device:
 
     def createComponent(self):
         # s contains the entire component in a single string
+
+        ports = {}
+        pincount = 0
+        portcount = 0
+        # Count amount of different Ports and how many I/O pins they contain
+        for pin in self.pins:
+            if(pin.pintype == "I/O" and len(pin.name) == 3):    # Avoid counting oscillator pins
+                pincount += 1
+                port = pin.name[1]
+                try:
+                    ports[port] += 1    # Increment pincount for this port
+                except KeyError:
+                    ports[port] = 1 # Key doesn't yet exist, create it
+                    portcount += 1
+        
+        padding = 200   # 200 mils top and bottom as padding
+        boxheight = pincount * 100 + (portcount - 1) * 100 + padding * 2   # height in mils 
+        
+        
         s = ""
-
-
-
-
+        s += "#\r\n"
+        s += "# " + self.name.upper() + "\r\n"
+        s += "#\r\n"
+        s += "DEF " + self.name + " U 0 40 Y Y 1 L N\r\n"
+        s += "F0 \"U\" -1550 1600 50 H V C CNN\r\n"
+        s += "F1 \"" + self.name + "\" 1300 -1600 50 H V C C\r\n"
+        s += "F2 \"~\" 0 -100 50 H V C CIN\r\n"
+        s += "F3 \"~\" 0 0 50 H V C CNN\r\n"
+        s += "DRAW\r\n"
+        # Start drawing rectangles and pins
+        s += "S -100 -" + str(round(boxheight/2)) + " 100 " + str(round(boxheight/2)) + " 0 1 10 f\r\n"
+        
+        s += "ENDDRAW\r\n"
+        s += "ENDDEF\r\n"
 
         self.componentstring = s
-            
+        print(s)  
 
 def main():
     args = sys.argv
-    ns = "{http://mcd.rou.st.com/modules.php?name=mcu}"
     
     pins = []
 
@@ -60,8 +89,12 @@ def main():
         printHelp()
     elif(os.path.isfile(args[1])):
         test = device(args[1])
-        
-
+        f = open("test.lib", "w")
+        header = '''EESchema-LIBRARY Version 2.3
+#encoding utf-8
+'''
+        f.write(header + test.componentstring + "#\r\n# End Library\r\n")
+        f.close()
     else:
         printHelp()
     
