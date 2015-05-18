958240a718ec52d396b06249386685eb435de7d3
Ricardo Crudo <ricardo.crudo@gmail.com>
2015-05-18 23:17:02 +0200

    pcb: small improvement to rule 6.3

--- pcb/checkrule6_3.py
+++ pcb/checkrule6_3.py
@@ -5,6 +5,11 @@ def get_pad_1(module):
         if pad['number'] == 1:
             return pad
 
+def set_anchor(pads_array, anchor_point):
+    for pad in pads_array:
+        pad['pos']['x'] -= anchor_point['x']
+        pad['pos']['y'] -= anchor_point['y']
+
 # returns true if a violation is found
 def check_rule(module):
     # check if module is through-hole
@@ -23,6 +28,4 @@ def fix_rule(module):
     xref = pad1['pos']['x']
     yref = pad1['pos']['y']
 
-    for pad in module.pads:
-        pad['pos']['x'] -= xref
-        pad['pos']['y'] -= yref
+    set_anchor(module.pads, {'x':xref , 'y':yref})
