160e7227b36092475822abe620b7d2ecba987cfb
Ricardo Crudo <ricardo.crudo@gmail.com>
2015-05-30 20:58:29 +0200

    pcb: created KLC rule base class

---
+++ pcb/rule.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class KLCRule(object):
+    """
+    A base class to represent a KLC rule
+    """
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def check(self, module):
+        raise NotImplementedError('The check method must be implemented')
+
+    def fix(self, module):
+        raise NotImplementedError('The fix method must be implemented')
