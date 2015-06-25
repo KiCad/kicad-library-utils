01f698652a17f67b935684a07c248275e6b13334
Ricardo Crudo <ricardo.crudo@gmail.com>
2015-06-25 21:28:16 +0200

    schlib: EC03 changed to check footprint and adjusting fields positions

--- schlib/rules/EC03.py
+++ schlib/rules/EC03.py
@@ -7,7 +7,7 @@ class Rule(KLCRule):
     Create the methods check and fix to use with the kicad lib files.
     """
     def __init__(self, component):
-        super(Rule, self).__init__(component, 'EC03 - Extra Checking', 'Check part reference and name position and alignment')
+        super(Rule, self).__init__(component, 'EC03 - Extra Checking', 'Check part reference, name and footprint position and alignment')
 
     def check(self):
         """
@@ -17,6 +17,8 @@ class Rule(KLCRule):
             * recommended_ref_alignment
             * recommended_name_pos
             * recommended_name_alignment
+            * recommended_fp_pos
+            * recommended_fp_alignment
         """
 
         # check if component has just one rectangle
@@ -29,13 +31,13 @@ class Rule(KLCRule):
 
         # if there is no pins in the top, the recommended position to ref is at top-center, horizontally centered
         if len(self.component.filterPins(direction='D')) == 0:
-            self.recommended_ref_pos = (0, top + 50)
+            self.recommended_ref_pos = (0, top + 125)
             self.recommended_ref_alignment = 'C'
 
-        # otherwise, the recommended is put it 50 mils before the first pin x position, right-aligned
+        # otherwise, the recommended is put it before the first pin x position, right-aligned
         else:
             x = min([int(i['posx']) for i in self.component.filterPins(direction='D')]) - 100
-            self.recommended_ref_pos = (x, top + 50)
+            self.recommended_ref_pos = (x, top + 125)
             self.recommended_ref_alignment = 'R'
 
         # get the current reference infos and compare them to recommended ones
@@ -47,16 +49,16 @@ class Rule(KLCRule):
 
         ## name checking
 
-        # if there is no pins in the bottom, the recommended position to name is at bottom-center, horizontally centered
-        if len(self.component.filterPins(direction='U')) == 0:
-            self.recommended_name_pos = (0, bottom - 50)
+        # if there is no pins in the top, the recommended position to name is at top-center, horizontally centered
+        if len(self.component.filterPins(direction='D')) == 0:
+            self.recommended_name_pos = (0, top + 50)
             self.recommended_name_alignment = 'C'
 
-        # otherwise, the recommended is put it 50 mils after the last pin x position, left-aligned
+        # otherwise, the recommended is put it before the first pin x position, right-aligned
         else:
-            x = max([int(i['posx']) for i in self.component.filterPins(direction='U')]) + 50
-            self.recommended_name_pos = (x, bottom - 50)
-            self.recommended_name_alignment = 'L'
+            x = min([int(i['posx']) for i in self.component.filterPins(direction='D')]) - 100
+            self.recommended_name_pos = (x, top + 50)
+            self.recommended_name_alignment = 'R'
 
         # get the current name infos and compare them to recommended ones
         name_need_fix = False
@@ -65,7 +67,27 @@ class Rule(KLCRule):
             self.component.fields[1]['htext_justify'] != self.recommended_name_alignment):
             name_need_fix = True
 
-        return True if (ref_need_fix or name_need_fix) else False
+        ## footprint checking
+
+        # if there is no pins in the bottom, the recommended position to footprint is at bottom-center, horizontally centered
+        if len(self.component.filterPins(direction='U')) == 0:
+            self.recommended_fp_pos = (0, bottom - 50)
+            self.recommended_fp_alignment = 'C'
+
+        # otherwise, the recommended is put it after the last pin x position, left-aligned
+        else:
+            x = max([int(i['posx']) for i in self.component.filterPins(direction='U')]) + 50
+            self.recommended_fp_pos = (x, bottom - 50)
+            self.recommended_fp_alignment = 'L'
+
+        # get the current footprint infos and compare them to recommended ones
+        fp_need_fix = False
+        pos = (int(self.component.fields[2]['posx']), int(self.component.fields[2]['posy']))
+        if (pos != self.recommended_fp_pos or
+            self.component.fields[2]['htext_justify'] != self.recommended_fp_alignment):
+            fp_need_fix = True
+
+        return True if (ref_need_fix or name_need_fix or fp_need_fix) else False
 
     def fix(self):
         """
@@ -79,3 +101,7 @@ class Rule(KLCRule):
             self.component.fields[1]['posx'] = str(self.recommended_name_pos[0])
             self.component.fields[1]['posy'] = str(self.recommended_name_pos[1])
             self.component.fields[1]['htext_justify'] = self.recommended_name_alignment
+
+            self.component.fields[2]['posx'] = str(self.recommended_fp_pos[0])
+            self.component.fields[2]['posy'] = str(self.recommended_fp_pos[1])
+            self.component.fields[2]['htext_justify'] = self.recommended_fp_alignment
