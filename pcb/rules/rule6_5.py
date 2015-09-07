787cb02675ecb29f3e90c01f6f68d6b92eda5335
Pablo <andres53016@gmail.com>
2015-09-07 06:02:06 -0300

    add part of check for rule 6.5->Silkscreen is not superposed to pads

--- pcb/rules/rule6_5.py
+++ pcb/rules/rule6_5.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
 from rules.rule import *
-
+import cmath
 class Rule(KLCRule):
     """
     Create the methods check and fix to use with the kicad_mod files.
@@ -23,11 +22,40 @@ class Rule(KLCRule):
 
         # check the width
         self.bad_width = []
+        
+        
         for graph in (self.f_silk + self.b_silk):
             if graph['width'] != 0.15:
                 self.bad_width.append(graph)
-
-        return True if len(self.bad_width) else False
+        # check intersections between line and pad, translate the line and pad to coordinate (0,0), rotate the line and pad 
+        self.intersections=[]
+        
+        for graph in (self.f_silk + self.b_silk):
+            if 'angle' in graph:
+                #TODO
+                pass
+            elif 'center' in graph:
+                #TODO
+                pass
+            else:
+                for pad in module.pads:
+                    padComplex=complex(pad['pos']['x'],pad['pos']['y'])
+                    startComplex=complex(graph['start']['x'],graph['start']['y'])
+                    endComplex=complex(graph['end']['x'],graph['end']['y'])
+                    if endComplex.imag>startComplex.imag:
+                        vector=endComplex-startComplex
+                        padComplex=padComplex-startComplex
+                    else:
+                        vector=startComplex-endComplex
+                        padComplex=padComplex-endComplex
+                    length=abs(vector)
+                    vectorR=cmath.rect(1,-cmath.phase(vector))
+                    padComplex=padComplex*vectorR
+                    if (padComplex.real-pad['size']['x']/2)<length and (padComplex.real+pad['size']['x']/2)>0:
+                        if abs(padComplex.imag)<pad['size']['x']/2+0.075:
+                            self.intersections.append([pad,graph])
+        return True if (len(self.bad_width) or len(self.intersections)) else False
+    
 
     def fix(self):
         """
