f5dcf58397e6d7101726332e33a6db194db68ecb
diggit <patrikbachan@gmail.com>
2016-03-06 15:37:55 +0100

    Add utils ,position formating and recheck functions
    New funtions to convert elements properties to human readable form.
    Covering pin_type electrical_type and fill.
    Recheck function runs check again and tells if everything got fixed.
    PositionFormater unifies coversion if position coordinates to string.

--- schlib/rules/rule.py
+++ schlib/rules/rule.py
@@ -1,5 +1,55 @@
 # -*- coding: utf-8 -*-
 
+#this should go to separate file
+def pinElecticalTypeToStr(pinEType):
+    pinMap={"I":"INPUT",\
+    "O":"OUTPUT",\
+    "B":"BIDI",\
+    "T":"TRISTATE",\
+    "P":"PASSIVE",\
+    "U":"UNSPECIFIED",\
+    "W":"POWER INPUT",\
+    "w":"POWER OUTPUT",\
+    "C":"OPEN COLLECTOR",\
+    "E":"OPEN EMITTER",\
+    "N":"NOT CONNECTED"}
+    if pinEType in pinMap.keys():
+        return pinMap[pinEType]
+    else:
+        return "INVALID"
+
+def pinTypeToStr(pinType):
+    pinMap={"I":"INVERTED",\
+    "C":"CLOCK",\
+    "CI":"INVERTED CLOCK",\
+    "L":"INPUT LOW",\
+    "CL":"CLOCK LOW",\
+    "V":"OUTPUT LOW",\
+    "F":"FALLING EDGE CLOCK",\
+    "X":"NON LOGIc"}
+    if pinType in pinMap.keys():
+        return pinMap[pinType]
+    else:
+        return "INVALID"
+
+def backgroundFillToStr(bgFill):
+    bgMap={
+    "F":"FOREGROUND",
+    "f":"BACKGROUND",
+    "N":"TRANSPARENT"}
+    if bgFill in bgMap.keys():
+        return bgMap[bgFill]
+    else:
+        return "INVALID"
+
+def positionFormater(element):
+    if type(element) != type({}):
+        raise Exception("input type: ",type(element),"expected dictionary, ",element)
+    if(not {"posx","posy"}.issubset(element.keys())):
+        raise Exception("missing keys 'posx' and 'posy' in"+str(element))
+    return "posx {0}, posy {1}".format(element['posx'],element['posy'])
+    # return "pos [{0},{1}]".format(element['posx'],element['posy'])
+
 class Verbosity:
     NONE=0
     NORMAL=1
@@ -31,3 +81,9 @@ class KLCRule(object):
 
     def fix(self, component):
         raise NotImplementedError('The fix method must be implemented')
+
+    def recheck(self):
+        if self.check():
+            self.verboseOut(Verbosity.HIGH, Severity.ERROR,"...could't be fixed")
+        else:
+            self.verboseOut(Verbosity.HIGH, Severity.SUCCESS,"everything fixed")
