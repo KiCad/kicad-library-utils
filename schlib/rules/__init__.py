07389bc50f7d78e5d559fd1fb6e7a4592b0226fb
Oliver <oliver.henry.walters@gmail.com>
2016-08-25 23:28:48 +1000

    Added rule "3.9" which sanity checks the contents of the footprint field

--- schlib/rules/__init__.py
+++ schlib/rules/__init__.py
@@ -1,2 +1,2 @@
 
-__all__ = ["rule3_1", "rule3_2", "rule3_6", "rule3_8", "EC01", "EC02", "EC03", "EC04"]
+__all__ = ["rule3_1", "rule3_2", "rule3_6", "rule3_8", "rule3_9", "EC01", "EC02", "EC03", "EC04"]
