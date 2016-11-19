f68599ec7974b300bf9a9392a45f74ce3539aeaa
Ricardo Crudo <ricardo.crudo@gmail.com>
2016-11-19 20:17:24 -0200

    fix issue #61

--- schlib/rules/EC05.py
+++ schlib/rules/EC05.py
@@ -14,47 +14,56 @@ class Rule(KLCRule):
         Proceeds the checking of the rule.
         Determines if any symbol pins are duplicated
         """
-        
+
         #dict of pins
         pins = {}
-        
+
         #look for duplicate pins
         for pin in self.component.pins:
-        
-            pin_number = int(pin['num'])
+
+            pin_number = pin['num']
             pin_name = pin['name']
-            
+
             #Check if there is already a match for this pin number
             if pin_number in pins.keys():
                 pins[pin_number].append(pin)
             else:
                 pins[pin_number] = [pin]
-                
+
         duplicate = False
-                
+
         for number in pins.keys():
             pin_list = pins[number]
-            
+
             if len(pin_list) > 1:
                 duplicate = True
                 self.verboseOut(Verbosity.NORMAL, Severity.WARNING, "Pin {n} is duplicated".format(n=number))
-                
+
                 for pin in pin_list:
                     self.verboseOut(Verbosity.HIGH, Severity.WARNING, "{n} - {name} @ {x},{y}".format(
                         n = pin['num'],
                         name = pin['name'],
                         x = pin['posx'],
                         y = pin['posy']))
-            
-        
-        missing = False
-        
+
+        # convert pins numbers to integers
+        int_pins = []
+        for pin in pins.keys():
+            try:
+                int_pins.append(int(pin))
+            except ValueError:
+                return duplicate
+
+        if not int_pins:
+            return duplicate
+
         #check for missing pins within the range of pins
-        for i in range(1, max(pins.keys()) + 1):
-            if i not in pins:
+        missing = False
+        for i in range(1, max(int_pins) + 1):
+            if i not in int_pins:
                 self.verboseOut(Verbosity.NORMAL, Severity.WARNING, "Pin {n} is missing".format(n=i))
                 missing = True
-                
+
         return duplicate or missing
 
     def fix(self):
