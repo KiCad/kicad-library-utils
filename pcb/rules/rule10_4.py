48201ec51e6e08507d2d9a90f061a66d2b455731
Oliver <oliver.henry.walters@gmail.com>
2017-02-13 14:11:24 +1100

    Renamed 10.8 -> 10.4

--- pcb/rules/rule10_4.py
+++ pcb/rules/rule10_4.py
@@ -8,7 +8,7 @@ class Rule(KLCRule):
     Create the methods check and fix to use with the kicad_mod files.
     """
     def __init__(self, module):
-        super(Rule, self).__init__(module, 'Rule 10.8', '3D Shape ".wrl" files are named the same as their footprint and are placed in a folder named the same as the footprint library replacing the ".pretty" with ".3dshapes".')
+        super(Rule, self).__init__(module, 'Rule 10.4', '3D Shape ".wrl" files are named the same as their footprint and are placed in a folder named the same as the footprint library replacing the ".pretty" with ".3dshapes".')
 
     def check(self):
         """
@@ -25,7 +25,7 @@ class Rule(KLCRule):
         if len(module.models) == 0:
             return False
         elif len(module.models) > 1:
-            self.verbose_message=self.verbose_message+"More than one 3D models have been specified in the footprint!\n"
+            self.addMessage("More than one 3D models have been specified in the footprint!")
             return True
 
         model_file_path = module.models[0]['file']
@@ -37,16 +37,16 @@ class Rule(KLCRule):
 
 
         if self.model_file[0] != module.name:
-            self.verbose_message=self.verbose_message+"3D model filename '{0}' is different from footprint name '{1}'!\n".format(self.model_file[0], module.name)
+            self.addMessage("3D model filename '{0}' is different from footprint name '{1}'!".format(self.model_file[0], module.name))
             ok=True
         if self.model_file[1] != '.wrl':
-            self.verbose_message=self.verbose_message+"3D model filename '{0}' is NOT a '.wrl' file!\n".format(self.model_file[1])
+            self.addMessage("3D model filename '{0}' is NOT a '.wrl' file!".format(self.model_file[1]))
             ok=True
         if self.model_dir[0] != self.module_dir[0]:
-            self.verbose_message=self.verbose_message+"3D model filename '{0}' is different from file directory '{1}'!\n".format(self.model_dir[0], self.module_dir[0])
+            self.addMessage("3D model filename '{0}' is different from file directory '{1}'!".format(self.model_dir[0], self.module_dir[0]))
             ok=True
         if self.model_dir[1] != '.3dshapes':
-            self.verbose_message=self.verbose_message+"3D model directory '{0}' is not a standard '3dshapes' directory!\n".format(self.model_dir[1])
+            self.addMessage("3D model directory '{0}' is not a standard '3dshapes' directory!".format(self.model_dir[1]))
             ok=True
         return ok
 
