1034b11a05f2a5d2377f799fab04a69449a3f4f3
Oliver <oliver.henry.walters@gmail.com>
2017-02-13 14:09:51 +1100

    Renamed 10.7 -> 10.3

--- pcb/rules/rule10_3.py
+++ pcb/rules/rule10_3.py
@@ -7,7 +7,7 @@ class Rule(KLCRule):
     Create the methods check and fix to use with the kicad_mod files.
     """
     def __init__(self, module):
-        super(Rule, self).__init__(module, 'Rule 10.7', 'All other properties are left to default values. (Move and Place: Free; Auto Place: 0 and 0,  Local Clearance Values: 0)')
+        super(Rule, self).__init__(module, 'Rule 10.3', 'All other properties are left to default values. (Move and Place: Free; Auto Place: 0 and 0,  Local Clearance Values: 0)')
 
     def check(self):
         """
@@ -16,28 +16,28 @@ class Rule(KLCRule):
         module = self.module
         ok=False
         if module.locked:
-            self.verbose_message=self.verbose_message+"Module is locked!\n"
+            self.addMessage("Module is locked!")
             ok=True
         if module.autoplace_cost90 != 0:
-            self.verbose_message=self.verbose_message+"Attribute autoplace_cost90 == {0} != 0!\n".format(module.autoplace_cost90)
+            self.addMessage("Attribute autoplace_cost90 == {0} != 0!".format(module.autoplace_cost90))
             ok=True
         if module.autoplace_cost180 != 0:
-            self.verbose_message=self.verbose_message+"Attribute autoplace_cost180 == {0} != 0!\n".format(module.autoplace_cost180)
+            self.addMessage("Attribute autoplace_cost180 == {0} != 0!".format(module.autoplace_cost180))
             ok=True
         if module.clearance != 0:
-            self.verbose_message=self.verbose_message+"Attribute clearance == {0} != 0!\n".format(module.clearance)
+            self.addMessage("Attribute clearance == {0} != 0!".format(module.clearance))
             ok=True
         if module.solder_mask_margin != 0:
-            self.verbose_message=self.verbose_message+"Attribute solder_mask_margin == {0} != 0!\n".format(module.solder_mask_margin)
+            self.addMessage("Attribute solder_mask_margin == {0} != 0!".format(module.solder_mask_margin))
             ok=True
         if module.solder_paste_margin != 0:
-            self.verbose_message=self.verbose_message+"Attribute solder_paste_margin == {0} != 0!\n".format(module.solder_paste_margin)
+            self.addMessage("Attribute solder_paste_margin == {0} != 0!".format(module.solder_paste_margin))
             ok=True
         if module.solder_paste_ratio != 0:
-            self.verbose_message=self.verbose_message+"Attribute solder_paste_ratio == {0} != 0!\n".format(module.solder_paste_ratio)
+            self.addMessage("Attribute solder_paste_ratio == {0} != 0!".format(module.solder_paste_ratio))
             ok=True
         if module.locked:
-            self.verbose_message=self.verbose_message+"Module is locked!\n"
+            self.addMessage("Module is locked!\n")
             ok=True
         
         return ok
