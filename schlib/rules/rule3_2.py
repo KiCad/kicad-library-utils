713b99b3d5b1138f4194938e1c117d32e97e37d4
Oliver <oliver.henry.walters@gmail.com>
2017-02-13 15:10:13 +1100

    EC04 -> 3.2

--- schlib/rules/rule3_2.py
+++ schlib/rules/rule3_2.py
@@ -7,7 +7,7 @@ class Rule(KLCRule):
     Create the methods check and fix to use with the kicad lib files.
     """
     def __init__(self, component):
-        super(Rule, self).__init__(component, 'EC04 - Extra Checking', 'Check line width and background for box outlines parts.')
+        super(Rule, self).__init__(component, '3.2', 'Symbol visual style')
 
     def check(self):
         """
