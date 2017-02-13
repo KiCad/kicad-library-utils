a2dd7d66d29568a68e93638897233c0505c789f2
Oliver <oliver.henry.walters@gmail.com>
2017-02-13 15:04:14 +1100

    3.8 -> 3.9

--- schlib/rules/rule3_9.py
+++ schlib/rules/rule3_9.py
@@ -7,7 +7,7 @@ class Rule(KLCRule):
     Create the methods check and fix to use with the kicad lib files.
     """
     def __init__(self, component):
-        super(Rule, self).__init__(component, 'Rule 3.8', 'Description and keywords properties contains information about the component.')
+        super(Rule, self).__init__(component, 'Rule 3.9', 'Part meta-data is filled out as appropriate')
 
     def check(self):
         """
