d8f5614793fc7fa0d6a0a0a28293da028a96f42f
Oliver Walters <oliver.henry.walters@gmail.com>
2017-02-27 22:45:02 +1100

    Search for illegal chars in filename

--- pcb/rules/rule10_1.py
+++ pcb/rules/rule10_1.py
@@ -10,20 +10,30 @@ class Rule(KLCRule):
     def __init__(self, module):
         super(Rule, self).__init__(module, 'Rule 10.1', 'Footprint name must match its filename')
 
+        self.illegal_chars = ['*', '?', ':', '/', '\\', '[', ']', ';', '|', '=', ',']
+        
     def check(self):
         """
         Proceeds the checking of the rule.
         """
+        
+        err = False
+        
         module = self.module
         if os.path.splitext(os.path.basename(module.filename))[0] != module.name:
             self.addMessage("footprint name (in file) was '{0}', but expected (from filename) '{1}'.\n".format(module.name, os.path.splitext(os.path.basename(module.filename))[0]))
-            return True
+            err = True
             
         if module.value['value'] != module.name:
             self.addMessage("Contents of values label ({lbl}) do not match filename".format(lbl=module.value))
-            return True
-        
-        return False
+            err = True
+            
+        for c in self.illegal_chars:
+            if c in module.name:
+                self.addMessage("Illegal character in filename: {c}".format(c=c))
+                err = True
+            
+        return Err
 
     def fix(self):
         """
