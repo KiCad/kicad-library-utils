53651cbe3ff9cf5af0a19c921706f5761babe73e
Oliver Walters <oliver.henry.walters@gmail.com>
2017-02-28 22:43:18 +1100

    Improvements for KLC10.4

--- pcb/rules/rule10_4.py
+++ pcb/rules/rule10_4.py
@@ -10,8 +10,45 @@ class Rule(KLCRule):
     Create the methods check and fix to use with the kicad_mod files.
     """
     def __init__(self, module, args):
-        super(Rule, self).__init__(module, args, 'Rule 10.4', '3D Shape ".wrl" files are named the same as their footprint and are placed in a folder named the same as the footprint library replacing the ".pretty" with ".3dshapes".')
+        super(Rule, self).__init__(module, args, 'Rule 10.4', '3D model reference')
 
+    def checkModel(self, model):
+    
+        error = False
+        
+        # Allowed model types
+        extensions = ["step","stp","wrl"]
+        
+        model = model['file']
+    
+        if model.startswith(SYSMOD_PREFIX):
+            model = model.replace(SYSMOD_PREFIX,"")
+            
+        try:
+            model_dir = os.path.dirname(model)
+            fn = os.path.basename(model).split(".")
+            model_file = ".".join(fn[:-1])
+            model_ext = fn[-1]
+        except:
+            self.addMessage("Model '{mod}' is invalid path".format(mod=model))
+            return True
+            
+        if not model_ext.lower() in extensions:
+            self.addMessage("Model '{mod}' is incompatible format (must be STEP or WRL file)".format(mod=model))
+            return True
+        
+        fp_dir = self.module_dir[0] + ".3dshapes"
+        fp_name = self.module.name
+        
+        if not model_dir == fp_dir:
+            self.addMessage("3D model directory is different from footprint directory (found '{n1}', should be '{n2}')".format(n1=model_dir, n2=fp_dir))
+            error = True
+        if not model_file == fp_name:
+            self.addMessage("3D model name is different from footprint name (found '{n1}', should be '{n2}')".format(n1=model_file, n2=fp_name))
+            error = True
+            
+        return error
+        
     def check(self):
         """
         Proceeds the checking of the rule.
@@ -24,51 +61,23 @@ class Rule(KLCRule):
         module_dir = os.path.split(os.path.dirname(os.path.realpath(module.filename)))[-1]
         self.module_dir = os.path.splitext(module_dir)
 
-        if len(module.models) == 0:
+        models = module.models
+        
+        if len(models) == 0:
+            # Warning msg
+            self.addMessage("Warning: No 3D model given")
             return False
-        elif len(module.models) > 1:
-            self.addMessage("More than one 3D models have been specified in the footprint!")
-            return True
-
-        model_file_path = module.models[0]['file']
         
-        if model_file_path.startswith(SYSMOD_PREFIX):
-            model_file_path = model_file_path.replace(SYSMOD_PREFIX, "")
-        
-        self.model_file = os.path.splitext(os.path.basename(model_file_path))
-        model_dir = os.path.split(os.path.dirname(model_file_path))[-1]
-        self.model_dir = os.path.splitext(model_dir)
-
-        ok=False
-
-
-        if self.model_file[0] != module.name:
-            self.addMessage("3D model filename '{0}' is different from footprint name '{1}'!".format(self.model_file[0], module.name))
-            ok=True
-        if self.model_file[1] != '.wrl':
-            self.addMessage("3D model filename '{0}' is NOT a '.wrl' file!".format(self.model_file[1]))
-            ok=True
-        if self.model_dir[0] != self.module_dir[0]:
-            self.addMessage("3D model filename '{0}' is different from file directory '{1}'!".format(self.model_dir[0], self.module_dir[0]))
-            ok=True
-        if self.model_dir[1] != '.3dshapes':
-            self.addMessage("3D model directory '{0}' is not a standard '3dshapes' directory!".format(self.model_dir[1]))
-            ok=True
-        return ok
+        if len(models) > 1:
+            self.addMessage("Warning: More than one 3D model given")
+            
+        model_error = False
+            
+        for model in models:
+            if self.checkModel(model):
+                model_error = True
+                
+        return model_error
 
     def fix(self):
-        """
-        Proceeds the fixing of the rule, if possible.
-        """
-        module = self.module
-        if self.check():
-            if len(module.models) == 1:
-                path = os.path.join(self.module_dir[0] + '.3dshapes', module.name + '.wrl')
-                
-                if module.models[0]['file'].startswith(SYSMOD_PREFIX):
-                    path = SYSMOD_PREFIX + path
-                
-                module.models[0]['file'] = path
-            elif len(module.models) > 1:
-                pass
-                # TODO
+        self.addFixMessage("Fix not supported")
