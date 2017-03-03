d8276a541c5281cf049ba804f53a5d4d6a286ba9
Oliver <oliver.henry.walters@gmail.com>
2017-03-03 16:57:02 +1100

    Update check_all.sh

--- pcb/travis/check_all.sh
+++ pcb/travis/check_all.sh
@@ -1,12 +1,9 @@
 SCRIPT="/home/travis/build/kicad-library-utils/pcb/check_kicad_mod.py"
 
 error=0
-# If IFS is left default (space) then any files with spaces will break the script
-IFS=$"\n"
 for change in $(git diff --name-only --diff-filter=AM $TRAVIS_COMMIT_RANGE); do
     echo "Checking: $change"
     python3 $SCRIPT "/$1/$change" -vv
     error="$(($error+$?))"
 done
-unset IFS
 exit $error
