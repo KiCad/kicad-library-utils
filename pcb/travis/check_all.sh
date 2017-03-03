216bfb8a33bf8179498f532bcbbed1841058b070
Oliver <oliver.henry.walters@gmail.com>
2017-03-03 16:04:17 +1100

    Fix git diff command to accommodate whitespace

--- pcb/travis/check_all.sh
+++ pcb/travis/check_all.sh
@@ -2,7 +2,7 @@ SCRIPT="/home/travis/build/kicad-library-utils/pcb/check_kicad_mod.py"
 
 error=0
 
-for change in $(git diff --name-only --diff-filter=AM $TRAVIS_COMMIT_RANGE); do
+for change in $(git diff -z --name-only --diff-filter=AM $TRAVIS_COMMIT_RANGE); do
     echo "$change"
     python3 $SCRIPT "$change" -vv
     error="$(($error+$?))"
