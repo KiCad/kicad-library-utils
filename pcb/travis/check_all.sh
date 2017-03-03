6945d9f6a6506f7ea150eb8d8a2d234f679d2126
Oliver <oliver.henry.walters@gmail.com>
2017-03-03 16:43:37 +1100

    Fixes for space characters

--- pcb/travis/check_all.sh
+++ pcb/travis/check_all.sh
@@ -1,11 +1,12 @@
 SCRIPT="/home/travis/build/kicad-library-utils/pcb/check_kicad_mod.py"
 
 error=0
-
-for change in $(git diff -z --name-only --diff-filter=AM $TRAVIS_COMMIT_RANGE); do
+# If IFS is left default (space) then any files with spaces will break the script
+IFS=$"\n"
+for change in $(git diff --name-only --diff-filter=AM $TRAVIS_COMMIT_RANGE); do
     echo "$change"
     python3 $SCRIPT "$change" -vv
     error="$(($error+$?))"
 done
-
+unset IFS
 exit $error
