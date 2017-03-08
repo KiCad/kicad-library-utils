9d68adf30e73bbb549b338c1c06711fd2d76b3b6
Oliver Walters <oliver.henry.walters@gmail.com>
2017-03-08 13:16:19 +1100

    Added some convenience functions

--- pcb/rules/rule.py
+++ pcb/rules/rule.py
@@ -6,6 +6,70 @@ sys.path.append("..\..\common")
 
 from rule import *
 
+# Convert mm to microns
+def mmToMicrons(mm):
+    if mm < 0:
+        mm -= 0.0000005
+    elif mm > 0:
+        mm += 0.0000005
+        
+    return int(mm * 1E6)
+
+def getStartPoint(graph):
+    if 'center' in graph:
+        return graph['center']
+    elif 'start' in graph:
+        return graph['start']
+    else:
+        return None
+        
+def getEndPoint(graph):
+    if 'end' in graph:
+        return graph['end']
+    else:
+        return None
+    
+# Display string for a graph item
+# Line / Arc / Circle
+def graphItemString(graph, layer=False, width=False):
+
+    layerText = ""
+    shapeText = ""
+    widthText = ""
+
+    try:
+        if layer:
+            layerText = " on layer '{layer}'".format(layer=graph['layer'])
+            
+        if width:
+            widthText = " has width '{width}'".format(width=graph['width'])
+    except:
+        pass
+        
+    # Line or Arc
+    if 'start' in graph and 'end' in graph:
+        # Arc item
+        if 'angle' in graph:
+            shape = 'Arc'
+        else:
+            shape = 'Line'
+        shapeText = "{shape} ({x1},{y1}) -> ({x2},{y2})".format(
+            shape = shape,
+            x1 = graph['start']['x'],
+            y1 = graph['start']['y'],
+            x2 = graph['end']['x'],
+            y2 = graph['end']['y'])
+            
+    # Circle
+    elif 'center' in graph and 'end' in graph:
+        shapeText = "Circle @ ({x},{y})".format(x=graph['center']['x'],y=graph['center']['y'])
+        
+    # Unkown shape
+    else:
+        shapeText = "Graphical item"
+        
+    return shapeText + layerText + widthText
+
 class KLCRule(KLCRuleBase):
     """
     A base class to represent a KLC rule
