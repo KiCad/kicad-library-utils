11d4d23b9ac809909604cfaabc70fa2b6d2ed151
Oliver <oliver.henry.walters@gmail.com>
2017-03-14 18:51:23 +1100

    Changed rule 3.x -> 4.x

--- schlib/rules/rule4_1.py
+++ schlib/rules/rule4_1.py
@@ -7,7 +7,7 @@ class Rule(KLCRule):
     Create the methods check and fix to use with the kicad lib files.
     """
     def __init__(self, component):
-        super(Rule, self).__init__(component, 'Rule 3.1 - Pin placement', 'Pins must be placed on regular grid, and be of correct length')
+        super(Rule, self).__init__(component, 'Rule 4.1 - Pin placement', 'Pins must be placed on regular grid, and be of correct length')
 
     def checkPinOrigin(self):
         self.violating_pins = []
--- schlib/rules/rule4_5.py
+++ schlib/rules/rule4_5.py
@@ -8,7 +8,7 @@ class Rule(KLCRule):
     Create the methods check and fix to use with the kicad lib files.
     """
     def __init__(self, component):
-        super(Rule, self).__init__(component, 'Rule 3.5 - Pin orientation', 'Wherever possible, pins should be arranged by function')
+        super(Rule, self).__init__(component, 'Rule 4.5 - Pin orientation', 'Wherever possible, pins should be arranged by function')
         
     def checkGroundPins(self):
     
