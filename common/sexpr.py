a37014e1df1b7cedb373068dacb948dc7859a9ec
Oliver <oliver.henry.walters@gmail.com>
2017-03-18 01:38:06 +1100

    Much improvement for sexpr generator

--- common/sexpr.py
+++ common/sexpr.py
@@ -42,20 +42,155 @@ def parse_sexp(sexp):
             raise NotImplementedError("Error: %r" % (term, value))
     assert not stack, "Trouble with nesting of brackets"
     return out[0]
-
-def build_sexp(exp):
+    
+class SexprBuilder(object):
+    def __init__(self, key, *arg, **kwarg):
+        
+        self.key = key
+        self.indent = kwargs.get('indent', 0)
+    
+        self.indent = kwargs.get('indent', 0)
+        self.key = None
+        self.output = ''
+        self.items = []
+        
+    # Move to new line (optionally indent)
+    def newline(self, indent=False):
+        self.output += "\n"
+        if indent:
+            self.indent += 1
+        self.output += 2 * self.indent * ' '
+        
+    def begin(self, key=None):
+        self.key = key
+        self.items = []
+        
+        self.output += '('
+        if key:
+            self.output += str(key)
+        
+    def add(self, item):
+        self.items.append(item)
+        
+    def end(self, newline=False):
+        self.output.append()
+    
+# Form a valid sexpr (single line)
+def SexprItem(val, key=None):
+    if key:
+        fmt = "(" + key + " {val})"
+    else:
+        fmt = "{val}"
+    
+    t = type(val)
+    
+    if t in [list, tuple]:
+        val = ' '.join([SexprItem(v) for v in val])
+    elif t == dict:
+        values = []
+        for key in val.keys():
+            values.append(SexprItem(val[key],key))
+        val = ' '.join(values)
+    #elif t == float:
+    #    val = float_render % val
+    elif t == str and re.search(r'[\s()\"]', val):
+        val = '"%s"' % repr(val)[1:-1].replace('"', '\"') 
+    
+    return fmt.format(val=val)
+    
+class SexprBuilder(object):
+    def __init__(self, key):
+        self.indent = 0
+        self.output = ''
+        self.items = []
+        if key is not None:
+            self.startGroup(key, newline=False)
+       
+    def _indent(self):
+        self.output += ' ' * 2 * self.indent
+   
+    def _newline(self):
+        self.output += '\n'
+        
+    def _addItems(self):
+        self.output += ' '.join(map(str,self.items))
+        self.items = []
+       
+    def startGroup(self, key=None, newline=True, indent=False):
+        self._addItems()
+        if newline and indent:
+            self.indent += 1
+        if newline:
+            self._newline()
+            self._indent()
+        self.output += '('
+        if key:
+            self.output += str(key) + ' '
+            
+    def endGroup(self, newline=True):
+        self._addItems()
+        if newline:
+            self._newline()
+            if self.indent > 0:
+                self.indent -= 1
+            self._indent()
+        self.output += ')'
+        
+    def addOptItem(self, key, item, newline=True, indent=False):
+        if item in [None, 0, False]:
+            return
+            
+        self.addItems({key: item}, newline=newline, indent=indent)
+            
+    def addItem(self, item, newline=True, indent=False):
+        self._addItems()
+        if newline and indent:
+            self.indent += 1
+        if newline:
+            self.newLine()
+        self.items.append(SexprItem(item))
+            
+    # Add a (preformatted) item
+    def addItems(self, *arg, newline=True, indent=False):
+        self._addItems()
+        if indent:
+            self.indent += 1
+        if newline:
+            self.newLine()
+        for a in arg:
+            self.items.append(SexprItem(a))
+            
+    def newLine(self, indent=False):
+        self._addItems()
+        self._newline()
+        if indent:
+            self.indent += 1
+        self._indent()
+        
+    def unIndent(self):
+        if self.indent > 0:
+            self.indent -= 1
+        
+def build_sexp(exp, key=None):
     out = ''
+    
+    # Special case for multi-values
     if type(exp) == type([]):
-        out += '(' + ' '.join(build_sexp(x) for x in exp) + ')'
+        out += '('+ ' '.join(build_sexp(x) for x in exp) + ')'
+        return out
     elif type(exp) == type('') and re.search(r'[\s()]', exp):
         out += '"%s"' % repr(exp)[1:-1].replace('"', '\"')
-    elif type(exp) == float:
+    elif type(exp) in [int,float]:
         out += float_render % exp
     else:
         if exp == '':
             out += '""'
         else:
             out += '%s' % exp
+    
+    if key is not None:
+        out = "({key} {val})".format(key=key, val=out)
+        
     return out
 
 def format_sexp(sexp, indentation_size=2, max_nesting=2):
