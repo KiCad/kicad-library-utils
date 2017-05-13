2397d7418bde81a035ff63e427b476c8e16b9070
hackscribble <hackscribble@outlook.com>
2017-05-13 18:13:10 +0100

    Extended to all .pretty folders in defined path.

--- pcb/check_3d_coverage.py
+++ pcb/check_3d_coverage.py
@@ -20,7 +20,7 @@ class Config:
         self.pretty_path = '../../'
         # self.pretty_path = '/home/ray/KiCad Contributing/'
         self.pretty = 'Housings_SOIC'
-        self.module_path = '{pp:s}{p:s}.pretty/'.format(pp=self.pretty_path, p=self.pretty)
+        self.module_path = self.pretty_path
         self.three_d_path = '../../kicad-library/modules/packages3d/'
         # self.three_d_path = '/home/ray/KiCad Contributing/kicad-library/modules/packages3d/'
         self.model_3D_path = '{pp:s}{p:s}.3dshapes/'.format(pp=self.three_d_path, p=self.pretty)
@@ -52,7 +52,51 @@ def parse_module(filename):
     except:
         printer.red("Invalid model reference: {f:s}".format(f=full))
         return None
-        
+
+
+def check_pretty(config, pretty):
+
+    references = []
+
+    printer.green('\r\nChecking: {p:s}\r\n'.format(p=pretty))
+    full_path = config.module_path + pretty
+    try:
+        for file in os.listdir(full_path):
+            if re.match('.*\.kicad_mod$', file):
+                reference = parse_module('{mp:s}/{mf:s}'.format(mp=full_path, mf=file))
+                if reference:
+                    references.append(ReferenceRecord(reference, file))
+                    # TODO add verbose control
+                    printer.green('Ref: {r:s} From: {m:s}'.format(r=reference, m=file)) 
+    except FileNotFoundError:
+        printer.red('Module path not found: {mp:s}'.format(mp=config.module_path))
+        sys.exit(1)
+
+    models = []
+
+    try:
+        for file in os.listdir(config.model_3D_path):
+            if re.match(config.regex_2, file):
+                models.append(file)
+    except FileNotFoundError:
+        printer.red('3D model path not found: {mp:s}'.format(mp=config.model_3D_path))
+        sys.exit(1)
+
+    unused = models[:]
+
+    for reference in references:
+        # TODO add verbose control
+        printer.green('3D model: {r:s}'.format(r=reference.model_3D))
+        if reference.model_3D in models:
+            if reference.model_3D in unused:
+                unused.remove(reference.model_3D)
+        else:
+            printer.red('No 3D model for reference {r:s} in module {m:s}'.format(r=reference.model_3D, m=reference.module))
+
+    for model in unused:
+        if re.match(config.regex_1, model):
+            printer.red('Unused 3D model: {m:s}'.format(m=model))
+
 
 # main program
 
@@ -62,44 +106,12 @@ printer = PrintColor(use_color=True)
 
 config = Config()
 
-references = []
-
-printer.green('\r\nChecking: {p:s}\r\n'.format(p=config.pretty))
-
 try:
-    for file in os.listdir(config.module_path):
-        if re.match('.*\.kicad_mod$', file):
-            reference = parse_module('{mp:s}{mf:s}'.format(mp=config.module_path, mf=file))
-            if reference:
-                references.append(ReferenceRecord(reference, file))
-                # TODO add verbose control
-                # printer.green('Ref: {r:s} From: {m:s}'.format(r=reference.model_3D, m=reference.module)) 
+    for folder in os.listdir(config.module_path):
+        # TODO handle like other regexes
+        if re.match('.*\.pretty$', folder):
+            check_pretty(config, folder)
 except FileNotFoundError:
     printer.red('Module path not found: {mp:s}'.format(mp=config.module_path))
     sys.exit(1)
-
-models = []
-
-try:
-    for file in os.listdir(config.model_3D_path):
-        if re.match(config.regex_2, file):
-            models.append(file)
-except FileNotFoundError:
-    printer.red('3D model path not found: {mp:s}'.format(mp=config.model_3D_path))
-    sys.exit(1)
-
-unused = models[:]
-
-for reference in references:
-    # TODO add verbose control
-    printer.green('3D model: {r:s}'.format(r=reference.model_3D))
-    if reference.model_3D in models:
-        if reference.model_3D in unused:
-            unused.remove(reference.model_3D)
-    else:
-        printer.red('No 3D model for reference {r:s} in module {m:s}'.format(r=reference.model_3D, m=reference.module))
-
-for model in unused:
-    if re.match(config.regex_1, model):
-        printer.red('Unused 3D model: {m:s}'.format(m=model))
 
