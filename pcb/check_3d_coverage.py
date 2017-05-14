8f3b1ba0feec4b59b3f22c9e0a76e0947cbd5e17
hackscribble <hackscribble@outlook.com>
2017-05-14 15:31:05 +0100

    Basic version working now.  Need to add argument parser.

--- pcb/check_3d_coverage.py
+++ pcb/check_3d_coverage.py
@@ -6,26 +6,51 @@ import re
 from print_color import *
 
 
-
-
 class Config:
 
     def __init__(self):
-        # TODO change to a path to KiCad repo root, then sub-paths from there
-        # TODO fix ALL paths !!!
-        main_3D_ext = ['wrl']
-        other_3D_ext = ['step', 'stp']
-        self.regex_1 = '.*\{ext:s}$'.format(ext=main_3D_ext[0])
-        self.regex_2 = '.*\.({ext:s})$'.format(ext='|'.join(main_3D_ext + other_3D_ext))
-        # Accept both forward and backward slash characters in path
-        self.regex_3 = '[/\\\]([^/\\\]*\.({ext:s}))$'.format(ext='|'.join(main_3D_ext + other_3D_ext))
-        self.pretty_path = '../../'
-        # self.pretty_path = '/home/ray/KiCad Contributing/'
-        self.pretty = 'Housings_SOIC'
-        self.module_path = self.pretty_path
-        self.three_d_path = '../../kicad-library/modules/packages3d/'
-        # self.three_d_path = '/home/ray/KiCad Contributing/kicad-library/modules/packages3d/'
-        self.model_3D_path = '{pp:s}{p:s}.3dshapes/'.format(pp=self.three_d_path, p=self.pretty)
+
+        # "pretty" is a footprint library
+        # "pretty name" is the footprint library folder name without extension (e.g. 'Housings_SOIC')
+
+        # "root name" is a directory name from which longer paths are constructed
+        # "path name" includes directory name and file name with extension
+        # "base name" is file name with extension
+
+        # TODO add arguments for ...
+        self.verbose = True
+        self.print_colour = True
+
+        self.pretty_root = '/home/ray/KiCad Contributing/'
+        self.model_root = '/home/ray/KiCad Contributing/kicad-library/modules/packages3d/'
+
+    def model_dir_name(self, pretty_name):
+        return os.path.join(self.model_root, pretty_name + '.3dshapes/')
+
+    def footprint_dir_name(self, pretty_name):
+        return os.path.join(self.pretty_root, pretty_name + '.pretty/')
+
+    def valid_pretty_names(self):
+        try:
+            return sorted([f.split('.')[0] for f in os.listdir(self.pretty_root) if os.path.isdir(os.path.join(self.pretty_root, f)) and f.endswith('.pretty')])
+        except FileNotFoundError:
+            printer.red('EXIT: module root not found: {mr:s}'.format(mr=self.pretty_root))
+            sys.exit(1)
+
+    def valid_models(self, pretty_name):
+        try:
+            return sorted([model for model in os.listdir(self.model_dir_name(pretty_name)) if model.endswith(('wrl', 'step', 'stp'))])
+        except FileNotFoundError:
+            printer.red('EXIT: 3D model directory not found: {d:s}'.format(d=self.model_dir_name(pretty_name)))
+            sys.exit(1)
+
+    def valid_modules(self, pretty_name):
+        dir_name = self.footprint_dir_name(pretty_name)
+        try:
+            return sorted ([f for f in os.listdir(dir_name) if os.path.isfile(os.path.join(dir_name, f)) and f.endswith('.kicad_mod')])
+        except FileNotFoundError:
+            printer.red('EXIT: module directory not found: {d:s}'.format(d=dir_name))
+            sys.exit(1)
 
 
 class ReferenceRecord:
@@ -37,83 +62,62 @@ class ReferenceRecord:
 
 def parse_module(filename):
 
-    # TODO add verbose control
-    # printer.green('Parsing: {f:s}'.format(f=filename))
+    if config.verbose:
+        printer.green('Parsing: {f:s}'.format(f=filename))
     try:
         module = KicadMod(filename)
     except FileNotFoundError:
-        printer.red('Module file not found: {fn:s}'.format(fn=filename))
-        return None
+        printer.red('EXIT: module file not found: {fn:s}'.format(fn=filename))
+        sys.exit(1)
     try:
-        full = module.models[0]['file']
+        long_reference = module.models[0]['file']
     except IndexError:
-        printer.yellow("No model file specified: {fn:s}".format(fn=filename))
+        printer.yellow("- No model file specified: {fn:s}".format(fn=filename))
         return None
     try:
-        return re.search(config.regex_3, full).group(1)
+        # Accept both forward and backward slash characters in path
+        long_reference = '/'.join(long_reference.split('\\'))
+        return os.path.basename(long_reference)
     except:
-        printer.red("Invalid model reference: {f:s}".format(f=full))
+        printer.yellow("- Invalid model reference: {f:s}".format(f=full))
         return None
 
 
-def check_pretty(config, pretty):
+def check_footprint_library(pretty_name):
 
     references = []
 
-    printer.green('\r\nChecking: {p:s}\r\n'.format(p=pretty))
-    full_path = config.module_path + pretty
-    try:
-        module_files = [f for f in os.listdir(full_path) if os.path.isfile(os.path.join(full_path, f)) and f.endswith('.kicad_mod')]
-        for f in sorted(module_files):
-            reference = parse_module(os.path.join(full_path, f))
-            if reference:
-                references.append(ReferenceRecord(reference, f))
-                # TODO add verbose control
-                printer.green('Ref: {r:s} From: {m:s}'.format(r=reference, m=f)) 
-    except FileNotFoundError:
-        printer.red('Module path not found: {mp:s}'.format(mp=config.module_path))
-        sys.exit(1)
+    printer.green('\r\nChecking: {p:s}'.format(p=pretty_name))
+    for module in config.valid_modules(pretty_name):
+        model_ref = parse_module(os.path.join(config.footprint_dir_name(pretty_name), module))
+        if model_ref:
+            references.append(ReferenceRecord(model_ref, module))
+            if config.verbose:
+                printer.green('- Reference: {r:s}'.format(r=model_ref)) 
 
-    models = []
-
-    try:
-        for file in os.listdir(config.model_3D_path):
-        # TODO replace regexes with .endswith(('wrl', 'step', 'stp'))
-            if re.match(config.regex_2, file):
-                models.append(file)
-    except FileNotFoundError:
-        printer.red('3D model path not found: {mp:s}'.format(mp=config.model_3D_path))
-        sys.exit(1)
-
+    models = config.valid_models(pretty_name)
     unused = models[:]
 
     for reference in references:
-        # TODO add verbose control
-        printer.green('3D model: {r:s}'.format(r=reference.model_3D))
+        if config.verbose:
+            printer.green('3D model: {r:s}'.format(r=reference.model_3D))
         if reference.model_3D in models:
             if reference.model_3D in unused:
                 unused.remove(reference.model_3D)
         else:
-            printer.red('No 3D model for reference {r:s} in module {m:s}'.format(r=reference.model_3D, m=reference.module))
+            printer.yellow('- No 3D model for reference {r:s} in module {m:s}'.format(r=reference.model_3D, m=reference.module))
 
     unused_models = [model for model in unused if model.endswith('.wrl')]
     for model in unused_models:
-        printer.red('Unused 3D model: {m:s}'.format(m=model))
+        printer.yellow('Unused 3D model: {m:s}'.format(m=model))
 
 
 # main program
 
-# TODO add colour control
-# printer = PrintColor(use_color=not args.nocolor)
-printer = PrintColor(use_color=True)
-
 config = Config()
 
-try:
-    pretty_folders = [f for f in os.listdir(config.module_path) if os.path.isdir(os.path.join(config.module_path, f)) and f.endswith('.pretty')]
-    for folder in sorted(pretty_folders):
-        check_pretty(config, folder)
-except FileNotFoundError:
-    printer.red('Module path not found: {mp:s}'.format(mp=config.module_path))
-    sys.exit(1)
+printer = PrintColor(use_color=config.print_colour)
+
+for pretty in config.valid_pretty_names():
+    check_footprint_library(pretty)
 
