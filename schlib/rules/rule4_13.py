2a840365f18a7d02c770d2694ed150fdc2d109e2
jkriege2 <jan@jkrieger.de>
2017-06-06 07:15:25 +0200

    updated to new KLC-version (added KLC 4.13-script, checking footprints/FPFilters)

--- schlib/rules/rule4_13.py
+++ schlib/rules/rule4_13.py
@@ -8,11 +8,12 @@ class Rule(KLCRule):
     Create the methods check and fix to use with the kicad lib files.
     """
     def __init__(self, component):
-        super(Rule, self).__init__(component, 'Rule 4.13 - Power flag symbols', 'Power-flag symbols follow some special rules/KLC-exceptions')
+        super(Rule, self).__init__(component, 'Rule 4.13 - Power-flag symbols', 'Power-flag symbols follow some special rules/KLC-exceptions')
         self.makePinINVISIBLE=False
         self.makePinPowerInput=False
         self.fixTooManyPins=False
         self.fixPinSignalName=False
+        self.fixNoFootprint=False
 
     
     def check(self):
@@ -27,8 +28,8 @@ class Rule(KLCRule):
                 fail=True
                 self.fixTooManyPins=True
             else:
-                if (self.component.pins[0]['electrical_type'].lower() != 'w'):
-                    self.error("The pin in power-flag symbols has to be of a POWER-type")
+                if (self.component.pins[0]['electrical_type'] != 'W'):
+                    self.error("The pin in power-flag symbols has to be of a POWER-INPUT")
                     fail=True
                     self.makePinPowerInput=True
                 if (not self.component.pins[0]['pin_type'].startswith('N')):
@@ -39,6 +40,16 @@ class Rule(KLCRule):
                     self.error("The pin name ("+self.component.pins[0]['name']+") in power-flag symbols has to be the same as the component name ("+self.component.name+")")
                     fail=True
                     self.fixPinSignalName=True
+                # footprint field must be empty
+                if self.component.fields[2]['name']!='' and self.component.fields[2]['name']!='""':
+                    self.error("Graphical symbols have no footprint association (footprint was set to '"+self.component.fields[2]['name']+"')")
+                    fail=True
+                    self.fixNoFootprint=True
+                # FPFilters must be empty
+                if len(self.component.fplist)>0:
+                    self.error("Graphical symbols have no footprint filters")
+                    fail=True
+                    self.fixNoFootprint=True
         
         return fail
 
@@ -50,7 +61,7 @@ class Rule(KLCRule):
             self.info("FIX for too many pins in power-symbol not supported")
         if self.makePinPowerInput:
             self.info("FIX: switching pin-type to power-input")
-            self.component.pins[0]['electrical_type']='w'
+            self.component.pins[0]['electrical_type']='W'
         if self.makePinINVISIBLE:
             self.info("FIX: making pin invisible")
             self.component.pins[0]['pin_type']='N'+self.component.pins[0]['pin_type']
@@ -60,3 +71,7 @@ class Rule(KLCRule):
                 newname=self.component.name[1:len(self.component.name)]
             self.info("FIX: change pin name to '"+newname+"'")
             self.component.pins[0]['pin_type']='N'+self.component.pins[0]['pin_type']
+        if self.fixNoFootprint:
+            self.info("FIX empty footprint association and FPFilters")
+            self.component.fplist.clear()
+            self.component.fields[2]=''
