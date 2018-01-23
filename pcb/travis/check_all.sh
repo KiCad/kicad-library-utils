d2977470b2326be2e971129bf12d57c19e48a88a
Rene Pöschl <poeschlr@gmail.com>
2018-01-23 13:53:59 +0100

    Allow travis to check renamed files.

--- pcb/travis/check_all.sh
+++ pcb/travis/check_all.sh
@@ -1,7 +1,7 @@
 SCRIPT="/home/travis/build/kicad-library-utils/pcb/check_kicad_mod.py"
 
 error=0
-for change in $(git diff --name-only --diff-filter=AM $TRAVIS_COMMIT_RANGE); do
+for change in $(git diff --name-only --diff-filter=AMR $TRAVIS_COMMIT_RANGE); do
     echo "Checking: $change"
     python3 $SCRIPT "/$1/$change" -vv
     error="$(($error+$?))"
