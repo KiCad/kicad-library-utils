79fe0dc626228c7621381d439ca5f8ce5cb56387
Rene Pöschl <r.poeschl@student.tugraz.at>
2018-03-02 00:57:23 +0100

    Make error message more informative (made it clear that it is about 3d models)

--- pcb/rules/F9_3.py
+++ pcb/rules/F9_3.py
@@ -26,7 +26,7 @@ class Rule(KLCRule):
             error = True
             self.model3D_wrongOffset = True
 
-            self.error("Model offset different from "\
+            self.error("3D model offset different from "\
                 "{{'x': 0, 'y': 0, 'z': 0}}. "\
                 "Found {{'x': {o[x]:}, 'y': {o[y]:}, 'z': {o[z]:}}}"\
                 .format(o=model['pos']))
@@ -37,7 +37,7 @@ class Rule(KLCRule):
             error = True
             self.model3D_wrongRotation = True
 
-            self.error("Model rotation different from "\
+            self.error("3D model rotation different from "\
                 "{{'x': 0, 'y': 0, 'z': 0}}. "\
                 "Found {{'x': {r[x]:}, 'y': {r[y]:}, 'z': {r[z]:}}}"\
                 .format(r=model['rotate']))
@@ -48,7 +48,7 @@ class Rule(KLCRule):
             error = True
             self.model3D_wrongScale = True
 
-            self.error("Model scale different from "\
+            self.error("3D model scale different from "\
                 "{{'x': 1, 'y': 1, 'z': 1}}. "\
                 "Found {{'x': {s[x]:}, 'y': {s[y]:}, 'z': {s[z]:}}}"\
                 .format(s=model['scale']))
