88ba9a6c05a9414a352a602bddf67fb552d7d2b6
Rene Pöschl <r.poeschl@student.tugraz.at>
2019-02-06 20:53:15 +0100

    Add output for finding the commit range reported by travis

--- pcb/travis/check_all.sh
+++ pcb/travis/check_all.sh
@@ -1,5 +1,7 @@
 SCRIPT="/home/travis/build/kicad-library-utils/pcb/check_kicad_mod.py"
 
+echo "Commit range checked: $TRAVIS_COMMIT_RANGE"
+
 error=0
 for change in $(git diff --name-only --diff-filter=AMR $TRAVIS_COMMIT_RANGE); do
     echo "Checking: $change"
