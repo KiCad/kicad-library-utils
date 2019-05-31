1f6fc0fb6f67705fef5012634f550cd6eedfec58
Rene Pöschl <poeschlr@gmail.com>
2019-06-01 00:35:01 +0200

    test with two dots instead of three

--- pcb/travis/check_all.sh
+++ pcb/travis/check_all.sh
@@ -1,8 +1,10 @@
 SCRIPT="/home/travis/build/kicad-library-utils/pcb/check_kicad_mod.py"
 
 echo "Commit range checked: $TRAVIS_COMMIT_RANGE"
+test_two_dots=${TRAVIS_COMMIT_RANGE/.../..}
+echo "$test_two_dots"
 git --version
-git diff --name-only --diff-filter=AMR $TRAVIS_COMMIT_RANGE
+git diff --name-only --diff-filter=AMR $test_two_dots
 
 error=0
 for change in $(git diff --name-only --diff-filter=AMR $TRAVIS_COMMIT_RANGE); do
