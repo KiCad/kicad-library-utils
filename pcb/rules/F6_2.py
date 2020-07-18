0084a5f939beecef5952f4d9edc99a973882ee71
cp-aquila <carsten.presser@aquila-biolabs.de>
2020-07-18 05:20:41 +0000

    Improve F2.6, Get Center from Fab as well as Pads (#260)
    Not all footprints have the center of the body align with the center of the pads.
    In a lot of cases one wants the body to be centered as this is what the pick and
    place machine needs.
    The check does of course still fail for parts that have a pick and place center
    suggested that differs from both these centers

--- pcb/rules/F6_2.py
+++ pcb/rules/F6_2.py
@@ -3,6 +3,7 @@
 from __future__ import division
 
 from rules.rule import *
+from math import sqrt
 
 class Rule(KLCRule):
     """
@@ -15,28 +16,42 @@ class Rule(KLCRule):
         """
         Proceeds the checking of the rule.
         The following variables will be accessible after checking:
-            * pads_bounds
-            * pads_distance
-            * right_anchor
+            * center_pads
+            * center_fab
+
+        This test will generate false positives for parts that have a weird outline, are not symmetrical or do not have 
+        the pick'n'place location in the geometrical center.
+        Most connectors will have at least one of those issues (e.g. a FPC-Connector) and fail this test.
+        For most other components (LEDs, molded packages, ...) this test will yield usable results.
         """
         module = self.module
         if module.attribute != 'smd':
             # Ignore non-smd parts
             return False
 
-        center = module.padMiddlePosition()
+        center_pads = module.padMiddlePosition()
+        center_fab  = module.geometricBoundingBox("F.Fab").center
 
         err = False
 
-        THRESHOLD = 0.001
-        x = center['x']
-        y = center['y']
+        # calculate the distance from origin for the pads and fab
+        diff_pads = sqrt(center_pads['x']**2 + center_pads['y']**2)
+        diff_fab  = sqrt(center_fab['x']**2 +  center_fab['y']**2)
+        # select the xy coordinates that are closest to the center
+        if diff_pads > diff_fab:
+            (x, y) = (center_fab['x'], center_fab['y'])
+        else:
+            (x, y) = (center_pads['x'], center_pads['y'])
 
+        THRESHOLD = 0.001
         if abs(x) > THRESHOLD or abs(y) > THRESHOLD:
-            self.error("Footprint anchor is not located at center of footprint")
-            self.errorExtra("Footprint center calculated as ({x},{y})mm".format(
-                x = round(center['x'], 5),
-                y = round(center['y'], 5)))
+            self.error("Footprint anchor does not match calculated center of Pads or F.Fab")
+            self.errorExtra("calculated center for Pads [{xp},{yp}mm]".format(
+                xp = round(center_pads['x'], 5),
+                yp = round(center_pads['y'], 5)))
+            self.errorExtra("calculated center for F.Fab [{xf},{yf}mm]".format(
+                xf = round(center_fab['x'], 5),
+                yf = round(center_fab['y'], 5)))
 
             err = True
 
@@ -45,6 +60,7 @@ class Rule(KLCRule):
     def fix(self):
         """
         Proceeds the fixing of the rule, if possible.
+        This fix will always use the pads center position.
         """
         module = self.module
         if self.check():
